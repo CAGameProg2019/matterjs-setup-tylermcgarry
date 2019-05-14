a0be1172b3466a0a6ceb80157f841adce56dd862
CA <bstumpf@concordacademy.org>
2019-05-14 01:04:02 -0400

    Added ball drop
    And set up the matter environment

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 // Write your code here
+let Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Composites = Matter.Composites;
+let engine = Engine.create();
+let render = Render.create({
+    element: document.body,
+    engine: engine
+});
+Engine.run(engine);
+Render.run(render);
+ 
+let ball=Bodies.circle(200, 10, 40);
+World.add(engine.world, [ball]);
