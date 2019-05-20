6770df351069f54bb421dbccd1c08f27a26d8428
tylermcgarry <46566991+tylermcgarry@users.noreply.github.com>
2019-05-20 11:40:36 -0400

    qwerty

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 // Write your code here
+
+
+
 let Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
@@ -7,16 +10,32 @@ let Engine = Matter.Engine,
 let engine = Engine.create();
 let render = Render.create({
     element: document.body,
-    engine: engine
+    engine: engine,
+	options: {
+		wireframes: false
+	}
+	
 });
 Engine.run(engine);
 Render.run(render);
- 
+
+
+
 let ball=Bodies.circle(200, 10, 40);
-let floor=Bodies.trapezoid(340,300,500,100,.9,{isStatic: true});
-let myCar=Composites.car(390, 0, 100, 30, 40);
-let myCradle=Composites.newtonsCradle(600, 100, 5, 10, 160);
-World.add(engine.world, [ball,floor,myCar,myCradle]);
+let floor=Bodies.rectangle(340,580,700, 30, {isStatic: true});
+let wall=Bodies.rectangle(0,580, 30, 1000, {isStatic: true});
+let wall2=Bodies.rectangle(675,580, 30, 1000, {isStatic: true});
+let pentagon = Bodies.polygon(450, 200, 6, 6,);
+let soft = Composites.softBody(200, 200, 20, 1, 1, 1, false, 5, null, null);
+
+let stuff = Composites.stack(20, 100, 17, 10, 1, 1, function(x, y){
+	return Bodies.rectangle(x, y, 36, 36);
+});
+							 
+//let myCar=Composites.car(390, 0, 100, 30, 40);
+let myCradle = Composites.newtonsCradle(600, 100, 30, 5, 160);
+
+World.add(engine.world, [floor, wall, wall2, stuff]);
  
 let world = engine.world;
 let Mouse= Matter.Mouse;
